f200a28714b4bd39abc11745f4eb48ed1f8061ea
reemo <r48n34@gmail.com>
2024-02-22 16:10:49 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,29 +15,36 @@ export default defineNuxtConfig({
     ],
     build: {
         transpile:
-          process.env.NODE_ENV === 'production'
-            ? [
-                'naive-ui',
-                'vueuc',
-                '@css-render/vue3-ssr',
-                '@juggle/resize-observer'
-              ]
-            : ['@juggle/resize-observer']
+            process.env.NODE_ENV === 'production'
+                ? [
+                    'naive-ui',
+                    'vueuc',
+                    '@css-render/vue3-ssr',
+                    '@juggle/resize-observer'
+                ]
+                : ['@juggle/resize-observer']
     },
     pwa: {
-      manifest: {
-        name: 'Nuxt Sushi',
-        short_name : 'Sushi',
-        background_color: '#3b3b3b',
-        lang: 'en',
-      }
+        manifest: {
+            name: 'Nuxt Sushi',
+            short_name: 'Sushi',
+            background_color: '#3b3b3b',
+            lang: 'en',
+        }
     },
     vite: {
         optimizeDeps: {
-          include:
-            process.env.NODE_ENV === 'development'
-              ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-              : []
+            include:
+                process.env.NODE_ENV === 'development'
+                    ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+                    : []
         }
     },
+    app: {
+        head: {
+          charset: 'utf-8',
+          viewport: 'width=device-width, initial-scale=1',
+          "google-site-verification": 'MSmy-J4XDNXLBlFKqvooWG7RzHPvXmjhSo1BWtgF40E',
+        }
+    }
 })
